4da88cd9d058b79372716977f78c5dec520a77dd
frsswq <farissaifuddin.work@gmail.com>
2024-11-22 19:48:36 +0700

    incorporate blog section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     "./index.html",
     "./work.html",
     "./contact.html",
+    "./404.html",
+    "./blog.html",
     "./work/*/**.html"
   ],
   theme: {
@@ -28,15 +30,3 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/forms"), require("preline/plugin")],
 };
-
-
-
-//$white: #ffffff !default;
-//$gray-300: #eaecec !default;
-//$gray-500: #bbbdbf !default;
-//$gray-700: #343535 !default;
-//$black: #050505 !default;
-
-//$blue: #2e59a9 !default;
-//$brown: #574a43 !default;
-//$orange: #e36c2a !default;
